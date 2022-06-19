12ca10d609521f0e08f6e4bf6ddbd6db685b075a
matheus-dr <m47heusdr@protonmail.com>
2022-06-19 14:34:16 -0300

    feat: adding http server

---
+++ src/infra/http/app.ts
@@ -0,0 +1,19 @@
+import cors from 'cors';
+import 'reflect-metadata';
+import express from 'express';
+import { config } from 'dotenv-flow';
+
+import api from '../../config/api';
+import { appRoutes } from './routes/app.routes';
+
+config({ silent: true });
+const apiConfig = api();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(apiConfig.BASE_URL, appRoutes);
+
+export { app };
---
+++ src/infra/http/routes/app.routes.ts
@@ -0,0 +1,5 @@
+import { Router } from 'express';
+
+const appRoutes = Router();
+
+export { appRoutes };
